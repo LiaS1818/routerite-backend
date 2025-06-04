6a1d11b8b69da7d4465ade3ae7f61ac7300eae44
Pablo Cardona <pablo@loroinsurance.com>
2025-06-04 11:51:46 -0600

    feat: Integración de Sequelize con PostgreSQL y configuración.
    - Se añadieron las dependencias @nestjs/config y @nestjs/sequelize.
    - Se creó DatabaseModule para gestionar la conexión a la base de datos mediante Sequelize.
    - Se añadió la configuración de la base de datos para PostgreSQL en database.config.ts.
    - Se actualizó AppModule para importar ConfigModule y DatabaseModule.
    - Se mejoró main.ts para habilitar CORS, establecer un prefijo global y usar canales de validación.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(process.env.PORT ?? 3000);
+	app.enableCors();
+	app.setGlobalPrefix('api');
+	app.useGlobalPipes(new ValidationPipe());
+	const port = process.env.PORT || 3000;
+	await app.listen(port);
+	console.log(`Application is running on: http://localhost:${port}/api`);
 }
 bootstrap();
