227614d3a61a339f2b9275bd9185642e770271cc
Pablo Cardona <pablo@loroinsurance.com>
2025-10-23 16:22:14 -0600

    Fix duplicated info

--- src/modules/places/places.module.ts
+++ src/modules/places/places.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { PlacesService } from './places.service';
 import { PlaceController } from './place.controller';
 import { PlacesSearchService } from './services/places-search.service';
 import { PlacesProcessorService } from './services/places-processor.service';
